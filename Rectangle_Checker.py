b78d2de552ab5e81fe95585e31157132665a8401
Wesley Burnawan <wesley.burnawan@gmail.com>
2022-05-22 00:47:50 -0500

    Added function

--- Rectangle_Checker.py
+++ Rectangle_Checker.py
@@ -1,19 +1,3 @@
-"""
-Modify points to a list of list containing x, y coordinates to coordinates
-Return number of x, y coordinates in coordinates
-"""
-def modify_input(points, coordinates):
-    size = 0
-    points = points.replace(" ", "")
-    for num in points:
-        if(len(coordinates[size]) < 2):
-            coordinates[size].append(int(num))
-        else:
-            coordinates.append([])
-            size += 1
-            coordinates[size].append(int(num))
-    return size
-
 #Check whether the input has the right format
 def check_input(points):
     points = points.replace(" ", "")
@@ -25,6 +9,39 @@ def check_input(points):
         return False
     return True
 
+#Modify points to a list of list containing x, y coordinates to coordinates
+def modify_input(points, coordinates):
+    size = 0
+    points = points.replace(" ", "")
+    for num in points:
+        if(len(coordinates[size]) < 2):
+            coordinates[size].append(int(num))
+        else:
+            coordinates.append([])
+            size += 1
+            coordinates[size].append(int(num))
+
+"""
+Check whether rectangle can be made from coordinates
+"""
+def form_rectangle(coordinates):
+    for p1 in range(len(coordinates)-3):
+        for p2 in range(p1+1, len(coordinates)-2):
+            for p3 in range(p2+1, len(coordinates)-1):
+                for p4 in range(p3+1, len(coordinates)):
+                    if is_rectangle(coordinates, p1, p2, p3, p4) == True:
+                        if points_on_rectangle(coordinates, p1, p2, p3, p4) == True:
+                            return True
+    return False
+
+
+#A helper function for form_rectangle to determine whether the 4 points create a rectangle
+def is_rectangle(coordinates, p1, p2, p3, p4):
+    pass
+
+#A helper function for form_rectangle to determine if all points in coordinates is contained on the rectangle
+def points_on_rectangle(coordinates, p1, p2, p3, p4):
+    pass
 
 #main function
 def main():
@@ -33,7 +50,11 @@ def main():
     'your coordinates are [(2,4), (3,5), (4,6), (5,7)], enter 2 4 3 5 4 6 5 7.\n')
     if check_input(points) == False:
         return
-    size = modify_input(points, coordinates)
+    modify_input(points, coordinates)
+    if form_rectangle(coordinates) == True:
+        print('Rectangle can be made from the coordinates given')
+    else:
+        print('Rectangle can\'t be made from the coordinates given')
 
 if __name__=='__main__':
     main()
